977fba3dcb5d2d25ad7d69d7a2a3d2986fc393f9
Kishor Naik <kishor.naik011.net@gmail.com>
2021-05-16 12:38:59 +0530

    Code Commit

---
+++ SqlServerLog/Sol_Demo/Sol_Demo/Program.cs
@@ -0,0 +1,37 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sol_Demo
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                                                                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                                                                .Build();
+
+            Log.Logger = new LoggerConfiguration()
+                                    .ReadFrom
+                                    .Configuration(configuration)
+                                    .CreateLogger();
+
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .UseSerilog()
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
